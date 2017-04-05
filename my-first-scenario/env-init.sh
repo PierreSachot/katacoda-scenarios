658a4a40edc8c14b5d1645889198af793ed7c26d
PierreSachot <sachot.pierre@laposte.net>
2017-04-05 10:59:42 +0100

    Update env-init.sh

--- my-first-scenario/env-init.sh
+++ my-first-scenario/env-init.sh
@@ -2,3 +2,4 @@ curl https://codeload.github.com/yannick-mayeur/january/zip/master > jan.zip
 unzip jan.zip
 curl https://codeload.github.com/yannick-mayeur/mvnTest/zip/master > yannick.zip
 unzip yannick.zip
+cp -r january-master/* .
