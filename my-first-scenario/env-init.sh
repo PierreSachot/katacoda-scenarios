af26bf4cf1ab544cd3103c1cb96a3d98bedbf6ae
PierreSachot <sachot.pierre@laposte.net>
2017-04-05 10:22:05 +0100

    Update env-init.sh

--- my-first-scenario/env-init.sh
+++ my-first-scenario/env-init.sh
@@ -1,8 +1,3 @@
-curl https://codeload.github.com/PierreSachot/katacoda-scenarios/zip/master > master.zip
-unzip master.zip
-cd /home/scrapbook/tutorial/katacoda-scenarios-master/my-first-scenario/Java
-javac Main.java
-java Main
 curl https://codeload.github.com/eclipse/january/zip/master > jan.zip
 unzip jan.zip
 curl https://codeload.github.com/yannick-mayeur/mvnTest/zip/master > yannick.zip
